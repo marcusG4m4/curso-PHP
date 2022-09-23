036ff15aaf74de3411b1d3ac771f11f05771d3a5
marcusG4m4 <sucramgama@gmail.com>
2022-09-23 19:45:04 -0300

    Olá PHP

---
+++ basico/ola.php
@@ -0,0 +1,14 @@
+<div class="titulo">Olá PHP</div>
+
+<h2>Exemplo A</h2>
+<?php
+echo 'Olá ';
+echo 'mundo!';
+?>
+<br>
+<h2>Exemplo B</h2>
+<?= "outra formar de me 'expressar!'" ?>
+
+<?php 
+phpinfo();
+?>
